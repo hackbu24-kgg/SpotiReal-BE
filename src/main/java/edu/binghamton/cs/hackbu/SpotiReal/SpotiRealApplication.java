d156b05c0597d697ee98c134724764be85d2544d
Gregory Maldonado <gmaldona@oswego.edu>
2024-02-17 13:09:14 -0500

    init

---
+++ src/main/java/edu/binghamton/cs/hackbu/SpotiReal/SpotiRealApplication.java
@@ -0,0 +1,13 @@
+package edu.binghamton.cs.hackbu.SpotiReal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotiRealApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotiRealApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/binghamton/cs/hackbu/SpotiReal/SpotiRealApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.binghamton.cs.hackbu.SpotiReal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotiRealApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
