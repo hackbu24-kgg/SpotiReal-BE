a21e5a801ce9e38ade992c5a96a789a54f5637f6
syoung64 <syoung5@binghamton.edu>
2024-02-17 16:33:01 -0500

    fixed get user info thing

--- src/main/java/edu/binghamton/cs/hackbu/SpotiReal/SpotiRealUtil.java
+++ src/main/java/edu/binghamton/cs/hackbu/SpotiReal/SpotiRealUtil.java
@@ -6,7 +6,7 @@ public class SpotiRealUtil {
 
     public static String getClientID() throws IOException, FileNotFoundException
     {
-        File file = new File("..\\..\\..\\..\\..\\..\\..\\..\\client.id");
+        File file = new File("client.id");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String clientid = br.readLine();
         br.close();
@@ -15,7 +15,7 @@ public class SpotiRealUtil {
 
     public static String getClientSecret() throws IOException, FileNotFoundException
     {
-        File file = new File("..\\..\\..\\..\\..\\..\\..\\..\\client.secret");
+        File file = new File("client.secret");
         BufferedReader br = new BufferedReader(new FileReader(file));
         String secret = br.readLine();
         br.close();
